d78ec7dc82b59dc4a9d25eda48b847589395f3ac
Greez <62405360+Wronghand-hash@users.noreply.github.com>
2025-08-01 22:43:15 +0330

    index setup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,44 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
 import pumpfunRoutes from "./pumpfun/pumpfun.handler";
 import { Router } from "express";
 import launchlabRouter from "./raydium/router/launchlab.router";
+import { NextFunction, Request, Response } from "express";
 
 dotenv.config();
 
 const app = express();
+
+app.use(helmet());
+
+// Configure CORS with specific origins
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [
+  "http://localhost:3000",
+];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
+
+// --- Router Setup ---
 const router = Router();
 router.use("/api", launchlabRouter);
 router.use("/api", pumpfunRoutes);
-app.use(express.json());
 app.use(router);
 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+// --- Server Start ---
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
